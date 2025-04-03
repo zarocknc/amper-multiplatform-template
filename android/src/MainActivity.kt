9e5e99ec8134904055c879a8e3341ad7d2fd35e1
Sebastian Nicolas <developer@pss.pe>
2025-04-03 10:49:42 -0500

    initial android compose

---
+++ android/src/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.jetbrains.sample.app
+
+import App
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+
+// *****  UI from shared  ****** /
+//import androidx.compose.foundation.layout.Arrangement
+//import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.text.BasicText
+//import androidx.compose.material3.MaterialTheme
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Alignment
+//import androidx.compose.ui.Modifier
+
+
+//@Composable
+//fun Screen() {
+//    MaterialTheme() {
+//        Column(
+//            modifier = Modifier.fillMaxSize(),
+//            verticalArrangement = Arrangement.Center,
+//            horizontalAlignment = Alignment.CenterHorizontally,
+//        ) {
+//            BasicText("Hello, World!")
+//        }
+//    }
+//}
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            App()
+        }
+    }
+}
